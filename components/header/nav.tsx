dd4e5938d9912e06148357f1a56b7b8b4284e8cf
Kqan1 <kaaneskimarmara@gmail.com>
2024-05-08 22:45:52 +0300

    added nav cancel properity and new routes

--- components/header/nav.tsx
+++ components/header/nav.tsx
@@ -1,46 +1,54 @@
 "use client";
 import Link from "next/link";
-import { useParams, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { HTMLAttributes } from "react";
+import { useSession } from "next-auth/react";
 
 interface Routes {
     href: string;
     label: string;
     active: boolean;
+    cancel?: boolean;
 }
 
 export default function Nav({ ...props }: HTMLAttributes<HTMLAnchorElement>) {
     const pathname = usePathname();
+    const Session = useSession();
 
     const routes: Routes[] = [
         {
-            href: `/`,
-            label: "Overview",
-            active: pathname === `/`,
+            href: "/",
+            label: "Home",
+            active: pathname === "/",
         },
         {
-            href: `/projects`,
-            label: "Projects",
-            active: pathname === `/projects`,
+            href: `/accound/${Session.data?.user?.id}`,
+            label: "My Accound",
+            active: pathname === `/accound/${Session.data?.user?.id}`,
+            cancel: Session.data?.user ? false : true,
         }
     ];
 
     return (
         <div className="flex items-center gap-4">
             {routes.map((route) => (
-                <Link
-                    key={route.href}
-                    href={route.href}
-                    className={cn(
-                        'text-sm font-medium transition-colors hover:text-primary',
-                        route.active ? 'text-black dark:text-white' : 'text-muted-foreground'
-                    )}
-                    {...props}
-                >
-                    {route.label}
-                </Link>
+                <>
+                {route.cancel ? null : 
+                    <Link
+                        key={route.href}
+                        href={route.href}
+                        className={cn(
+                            'text-sm font-medium transition-colors hover:text-primary',
+                            route.active ? 'text-black dark:text-white' : 'text-muted-foreground'
+                        )}
+                        {...props}
+                    >
+                        {route.label}
+                    </Link>
+                    }
+                </>
             ))}
         </div>
     );
