526dc01a1a39530cf6432fc0870992cfeecc1fcc
Kqan1 <kaaneskimarmara@gmail.com>
2024-05-17 21:41:47 +0300

    added new route and fixed the key error

--- components/header/nav.tsx
+++ components/header/nav.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Fragment } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -20,35 +21,44 @@ export default function Nav({ ...props }: HTMLAttributes<HTMLAnchorElement>) {
     const routes: Routes[] = [
         {
             href: "/",
-            label: "Home",
+            label: "Ev",
             active: pathname === "/",
         },
         {
-            href: `/accound/${Session.data?.user?.id}`,
-            label: "My Accound",
-            active: pathname === `/accound/${Session.data?.user?.id}`,
-            cancel: Session.data?.user ? false : true,
+            href: "/follows",
+            label: "Takip Ettiklerim",
+            active: pathname === "/follows",
+        },
+        {
+            href: "/accound/posts",
+            label: "Gönderilerim",
+            active: pathname === "/follows",
+        },
+        {
+            href: `/account/${Session.data?.user?.id}`,
+            label: "Hesabım",
+            active: pathname === `/account/${Session.data?.user?.id}`,
+            cancel: !Session.data?.user,
         }
     ];
 
     return (
         <div className="flex items-center gap-4">
-            {routes.map((route) => (
-                <>
-                {route.cancel ? null : 
-                    <Link
-                        key={route.href}
-                        href={route.href}
-                        className={cn(
-                            'text-sm font-medium transition-colors hover:text-primary',
-                            route.active ? 'text-black dark:text-white' : 'text-muted-foreground'
-                        )}
-                        {...props}
-                    >
-                        {route.label}
-                    </Link>
-                    }
-                </>
+            {routes.map((route, index) => (
+                <Fragment key={route.href}>
+                    {!route.cancel && (
+                        <Link
+                            href={route.href}
+                            className={cn(
+                                'text-sm font-medium transition-colors hover:text-primary',
+                                route.active ? 'text-black dark:text-white' : 'text-muted-foreground'
+                            )}
+                            {...props}
+                        >
+                            {route.label}
+                        </Link>
+                    )}
+                </Fragment>
             ))}
         </div>
     );
